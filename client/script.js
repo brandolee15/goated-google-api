3b7e3f79c9389ce532a241175211560f0757d566
velinkalenderski <velinkalenderski@hotmail.co.uk>
2021-09-09 00:32:02 +0100

    link styling example

--- client/script.js
+++ client/script.js
@@ -1,13 +1,12 @@
 const searchBtn = document.getElementById('google-search')
 
-
 searchBtn.addEventListener('click', search)
 
 function search(e) {
     try {
     e.preventDefault();
-    const searchedTerm = document.getElementById('search').value
-    let searchArray = searchedTerm.split(' ')
+    const searchedTerm = document.getElementById('search').value;
+    let searchArray = searchedTerm.split(' ');
     
     console.log(searchedTerm)
     
@@ -16,34 +15,34 @@ function search(e) {
     //console.log(searchBtn)
     .then(resp => resp.json())
     .then(data => {
-        for (let k = 0; k<searchArray.length;k++) {
+        for (let k = 0; k < searchArray.length; k++) {
             sortResults(data, searchArray[k])
         }
     })
     
-    //.then(console.log(data[1]))
+    // .then(console.log(data[1]))
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
   }
 
   function sortResults(data,searchArray) {
     //console.log(typeOf(searchArray[0]))
-    let resData = data
-    console.log(searchArray)
-    for (let i=0;i<=9;i++) {
+    let resData = data;
+    console.log(searchArray);
+    for (let i = 0; i <= 9; i++) {
         console.log('it went through')
-        let str = resData[i]["description"]
-        let str2 = resData[i]["title"]
-        if (str.includes(searchArray) || str2.includes(searchArray) ) {
-            console.log('butter') 
-                resData[i].count++
+        let str = resData[i]["description"];
+        let str2 = resData[i]["title"];
+        if (str.includes(searchArray) || str2.includes(searchArray)) {
+            console.log('butter'); 
+                resData[i].count++;
             }
         else {
-            continue
+            continue;
         }
     }
-    
+    console.log(resData[0]);
   }
 
   
