be726a51d3458060b52a1ac09ce28bb5b383b5f2
SharenSella <sharensella@outlook.com>
2021-12-09 09:39:17 +0000

    getting the api and front end to work

---
+++ server/data.js
@@ -0,0 +1,15 @@
+const searchResults = [
+    {id: 1 , link: "https://www.instagram.com", title: "Instagram" , description: "Create an account or log in to Instagram - A simple, fun & creative way to capture, edit & share photos, videos & messages with friends & family.", count:0},
+    {id: 2 , link: "https://www.github.com", title: "GitHub: Where the world builds software" , description: "GitHub is where over 65 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories", count:0},
+    {id: 3 , link: "https://www.youtube.com", title: "Youtube" , description: "Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on YouTube.", count:0},
+    {id: 4 , link: "https://www.twitter.com", title: "See what's happening in the world right now - Twitter" , description: "Join Twitter today. Sign up Log In. Follow your interests. Hear what people are talking about. Join the conversation.", count:0},
+    {id: 5 , link: "https://www.w3schools.com", title: "W3Schools Online Web Tutorials" , description: "W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references", count:0},
+    {id: 6 , link: "https://www.spotify.com", title: "Spotify: Listening is everything" , description: "Enjoy ad-free music listening, offline playback, and more. Cancel anytime. ... Individual plan only. $9.99/month after. Terms and conditions apply.", count:0},
+    {id: 7 , link: "https://www.nike.com", title: "Nike. Just Do It. Nike GB" , description: "Inspiring the world's athletes, Nike delivers innovative products, experiences and services", count:0},
+    {id: 8 , link: "https://www.arsenal.com", title: "Arsenal FC Official Website | Home | Arsenal.com" , description: "Arsenal Football Club Official Website: get the latest club news, highlights, fixtures and results. Become a free digital member to get exclusive content.", count:0},
+    {id: 9 , link: "https://www.netflix.co.uk", title: "Netflix - Watch TV Shows Online, Watch Movies Online" , description: "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of", count: 0},
+    {id: 10 , link: "https://www.amazon.co.uk/", title: "Amazon.co.uk: Low Prices in Electronics, Books, Sports and more" , description: "Sign up to Amazon Prime for unlimited free delivery. Low prices at Amazon on digital cameras, MP3, sports, books, music, DVDs, video games, home & garden.", count: 0}
+]
+
+module.exports = searchResults
+
